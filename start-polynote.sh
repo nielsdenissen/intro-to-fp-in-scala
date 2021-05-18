0929395d19e9cae6902aa35aaee158c01de5a2ca
Niels Denissen <nielsdenissen@gmail.com>
2021-05-18 18:06:48 +0200

    Add exercises in Polynote

--- start-polynote.sh
+++ start-polynote.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -p 8192:8192 -v `pwd`/polynote_notebooks:/usr/src/app/polynote/notebooks --name=polynote -d -t greglinscheid/polynote:latest
+docker run -p 8192:8192 -v `pwd`/polynote_notebooks:/usr/src/app/polynote/notebooks --name=polynote --rm -d -t greglinscheid/polynote:latest
+
+echo "Open Polynote on: http://localhost:8192"
